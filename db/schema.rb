697dadde5b5253cf0cb4dcb866ff733dcd819491
Prashant Sharma <prashant080994@gmail.com>
2017-12-14 19:25:48 +0530

    implemented carrer wave gem for uploading file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214125431) do
+ActiveRecord::Schema.define(version: 20171214134133) do
 
   create_table "movies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171214125431) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "movie_id"
+    t.string "image"
   end
 
 end

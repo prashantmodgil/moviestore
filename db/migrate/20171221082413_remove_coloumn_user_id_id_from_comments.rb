d3fec880ff8f59aabd5dcc5826497176c5633396
Prashant Sharma <prashant080994@gmail.com>
2018-01-03 20:28:10 +0530

    migration fixxed

--- db/migrate/20171221082413_remove_coloumn_user_id_id_from_comments.rb
+++ db/migrate/20171221082413_remove_coloumn_user_id_id_from_comments.rb
@@ -1,5 +1,5 @@
-class RemoveColoumnUserIdIdFromComments < ActiveRecord::Migration[5.1]
-  def change
-#    remove_column :comments, :user_id_id, :integer
-  end
-end
+# class RemoveColoumnUserIdIdFromComments < ActiveRecord::Migration[5.1]
+#   def change
+# #    remove_column :comments, :user_id_id, :integer
+#   end
+# end

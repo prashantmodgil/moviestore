a0a9c53291cb64992563c32989afc3bc17cb27f1
Prashant Sharma <prashant080994@gmail.com>
2018-01-04 15:10:36 +0530

    changing in application mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { host: 'https://prashantdevelop.herokuapp.com/'}
     config.action_mailer.smtp_settings = {
       # enable_starttls_auto: true,
       address: 'smtp.gmail.com',
@@ -73,6 +74,7 @@ Rails.application.configure do
       password: Rails.application.secrets.password
      }
   config.action_mailer.perform_caching = false
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
